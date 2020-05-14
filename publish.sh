4a3f04b32df48f8e9557749fea659ce4edf0e6e0
yahgwai <cpbuckland88@gmail.com>
2020-05-14 23:23:40 +0200

    Added publish command

---
+++ publish.sh
@@ -0,0 +1,22 @@
+VERSION=$1
+TAG=${2:-latest}
+
+if [ "$#" -ne 2 ]
+then
+  echo 'Usage: Supply a version and a tag 
+       ./publish.sh (<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease | from-git) (tag)
+       
+       Eg: ./publish.sh prerelease next'
+  exit 1
+fi
+
+echo "Version : $VERSION"
+echo "Tag $TAG"
+
+pnpm run clean
+pnpm i --frozen-lockfile
+pnpm run build
+pnpm --filter . exec -- npm version $VERSION
+git commit -am "Version bump $VERSION"
+git push
+pnpm publish --access public --tag $TAG
