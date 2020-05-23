a53fffba259beaf3e0f28b7d4e13bfbee8366fbb
Patrick McCorry <stonecoldpat@gmail.com>
2020-05-23 19:23:42 +0100

    fixed globaldeployer typo. will add unit testing shortly in this PR.

--- src/contracts/ops/GlobalDeployer.sol
+++ src/contracts/ops/GlobalDeployer.sol
@@ -10,14 +10,14 @@ contract GlobalDeployer {
 
     /// @dev Deploys a contract via CREATE2.
     /// @param _initCode Bytecode for deployment
-    /// @param _extraData
+    /// @param _extraData Extra data for the salt. Salt is H(_extraData | msg.sender)
     function deploy(bytes memory _initCode, bytes32 _extraData) public payable
     {
         bytes32 salt = keccak256(abi.encodePacked(_extraData, msg.sender));
         address addr;
         uint value = msg.value;
         assembly {
-            addr := create2(value, add(initCode, 0x20), mload(initCode), salt)
+            addr := create2(value, add(_initCode, 0x20), mload(_initCode), salt)
         }
 
         require(addr != address(0), "CREATE2 failed to deploy.");
